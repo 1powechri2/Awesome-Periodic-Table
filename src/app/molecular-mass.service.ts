517274100344e153fbbc15192380f612c4621da7
Chris Powell <chris.ian.powell@gmail.com>
2019-02-07 18:36:50 -0700

    Adds service for molecular mass and molecular mass model and molecular mass component

--- src/app/molecular-mass.service.ts
+++ src/app/molecular-mass.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MolecularMassService {
+  url = 'https://chemistry-is-awesome.herokuapp.com/api/v1/molecular_mass?molecule=C20H25N3O'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMolecularMass() {
+    return this.http.get(this.url);
+  }
 }
--- src/app/molecular-mass/molecular-mass.component.ts
+++ src/app/molecular-mass/molecular-mass.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { molecularMass } from '../molecular_mass'
+import { MolecularMassService } from '../molecular-mass.service';
 
 @Component({
   selector: 'app-molecular-mass',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MolecularMassComponent implements OnInit {
 
-  constructor() { }
+  mole_mass: molecularMass;
+
+  constructor(private massService: MolecularMassService) { }
+
+  showMass() {
+    this.massService.getMolecularMass()
+    .subscribe((data: molecularMass) => this.mole_mass = {
+      molecule: data['molecule'],
+      molecular_mass: data['molecular_mass']
+    });
+  }
 
   ngOnInit() {
+    this.showMass();
+    console.log(this.mole_mass)
   }
 
 }
