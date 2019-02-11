f44db4bd66a9af0f5a7e1cbcc2bb65f60d22706b
Chris Powell <chris.ian.powell@gmail.com>
2019-02-11 11:25:49 -0700

    Adds element object to app root

---
+++ src/app/element.ts
@@ -0,0 +1,15 @@
+export class Element {
+  id: number;
+  number: number;
+  symbol: string;
+  appearance: string;
+  group_block: string;
+  period: number;
+  element_category: string;
+  atomic_weight: string;
+  electron_configuration: string;
+  phase: string;
+  melting_point: string;
+  boiling_point: string;
+  electronegativity: string;
+}
